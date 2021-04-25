13edd07e69fa67476dc8897d09f69bd4ab665cc5
hofstede-matheus <hofstede.matheus@gmail.com>
2021-04-24 23:15:01 -0300

    create mysql service

--- test.sh
+++ test.sh
@@ -2,8 +2,13 @@
 
 docker network create pfa
 
+docker run --network=pfa --name mysqldb -v "$PWD/mysql/data:/var/lib/mysql" -d -e MYSQL_ROOT_PASSWORD=wowsuchsecret -e MYSQL_DATABASE=pfa he4dless/pfa-docker-desafio1-mysql
 
+sleep 5
+
+docker exec -i mysqldb bash -c 'mysql -uroot -p"$MYSQL_ROOT_PASSWORD" pfa < seed.sql'                                                           
 
 docker run --network=pfa -d --name nodeapi he4dless/pfa-docker-desafio1-node
 
-docker run --network=pfa --name nginx -p 8080:80 he4dless/pfa-docker-desafio1-nginx
+docker run --network=pfa -d --name nginx -p 8080:80 he4dless/pfa-docker-desafio1-nginx
+
